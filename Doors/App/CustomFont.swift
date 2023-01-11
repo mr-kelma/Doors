2e2a84330ab178c29a2944d9ae7f54bdb9884a81
mr-kelma <mr.kelma@gmail.com>
2023-01-11 16:53:55 +0400

    Add custom font and set labels

---
+++ Doors/App/CustomFont.swift
@@ -0,0 +1,18 @@
+//
+//  CustomFont.swift
+//  Doors
+//
+//  Created by Valery Keplin on 11.01.23.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    static func customFont(size: CGFloat) -> UIFont {
+      guard let customFont = UIFont(name: "Sk-Modernist-Bold", size: size) else {
+        return UIFont.systemFont(ofSize: size)
+      }
+      return customFont
+    }
+}
--- Doors/MainViewController.swift
+++ Doors/MainViewController.swift
@@ -11,11 +11,32 @@ import SnapKit
 class MainViewController: UIViewController {
 
     // MARK: Property
-    
+
     //MARK: - UIElements
     
-    private let imageHomes = UIImage(named: "imageHomes")
-    private let topLabel = UIImage(named: "topLabel")
+    // LABELS
+    private let imageHomes = UIImageView(image: UIImage(named: "imageHomes"))
+    private let topLabel = UIImageView(image: UIImage(named: "topLabel"))
+    
+    private func setLabel(text: String, size: CGFloat) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.font = UIFont.customFont(size: size)
+        return label
+    }
+    private lazy var welcomeLabel = setLabel(text: "Welcome", size: 35)
+    private lazy var myDoorsLabel = setLabel(text: "My doors", size: 20)
+    
+    // BUTTON
+    private func settingButton(text: String, action: Selector) -> UIButton {
+        let button = UIButton()
+        button.setImage(UIImage(named: text), for: .normal)
+        button.clipsToBounds = true
+        button.addTarget(self, action: action, for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }
+    private lazy var settingButton = settingButton(text: "labelSetting", action: #selector(pressedSetting))
     
     // MARK: - LifeCycle
     
@@ -23,26 +44,40 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         initialize()
     }
-
     
     //MARK: - Setups
     
     private func initialize() {
         view.backgroundColor = UIColor(ciColor: .white)
+
+        view.addSubview(topLabel)
+        topLabel.snp.makeConstraints { maker in
+            maker.top.equalToSuperview().inset(77)
+            maker.left.equalToSuperview().inset(24)
+        }
         
-        let imageHomes = UIImageView(image: imageHomes)
-        let topLabel = UIImageView(image: topLabel)
+        view.addSubview(settingButton)
+        settingButton.snp.makeConstraints { maker in
+            maker.top.equalToSuperview().inset(63)
+            maker.right.equalToSuperview().inset(27)
+        }
         
         view.addSubview(imageHomes)
         imageHomes.snp.makeConstraints { maker in
-            maker.top.equalToSuperview().inset(120)
-            maker.right.equalToSuperview().inset(15)
+            maker.top.equalToSuperview().inset(108)
+            maker.right.equalToSuperview().inset(4)
         }
         
-        view.addSubview(topLabel)
-        topLabel.snp.makeConstraints { maker in
-            maker.top.equalToSuperview().inset(100)
-            maker.left.equalToSuperview().inset(15)
+        view.addSubview(welcomeLabel)
+        welcomeLabel.snp.makeConstraints { maker in
+            maker.top.equalToSuperview().inset(157)
+            maker.left.equalToSuperview().inset(24)
+        }
+        
+        view.addSubview(myDoorsLabel)
+        myDoorsLabel.snp.makeConstraints { maker in
+            maker.top.equalToSuperview().inset(307)
+            maker.left.equalToSuperview().inset(24)
         }
     }
     
@@ -51,5 +86,4 @@ class MainViewController: UIViewController {
     @objc private func pressedSetting() {
         print("The settings button was pressed")
     }
-    
 }
